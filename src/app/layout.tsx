783725b525b722abd6be3041646e21386a67bb8e
Karsen H <karsenhansen@gmail.com>
2024-11-20 10:59:06 -0800

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
-import type { Metadata } from "next";
-import {Inter, Calistoga } from 'next/font/google'
-import "./globals.css";
-import { twMerge } from "tailwind-merge";
+import type { Metadata } from 'next';
+import { Inter, Calistoga } from 'next/font/google';
+import './globals.css';
+import { twMerge } from 'tailwind-merge';
 
-const inter = Inter({subsets: ['latin'], variable: "--font-sans"})
-const calistoga = Calistoga({subsets: ['latin'], variable: '--font-serif', weight: ["400"]})
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+const calistoga = Calistoga({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400'],
+});
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Karsen's portfolio",
+  description: 'Created with NextJS',
 };
 
 export default function RootLayout({
@@ -17,8 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>{children}</body>
+    <html lang='en'>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          'bg-gray-900 text-white antialiased font-sans'
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
